7888f6a7145b593374c817614b1bd2b76ce66485
Kasper Wind <kasperwind79@gmail.com>
2023-10-01 16:28:28 +0200

    impl as one method on struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, BufRead, Write};
-use message::{HeaderMessage, Body};
+use message::HeaderMessage;
 
 
 mod message;
@@ -12,13 +12,7 @@ fn main() {
     handle.read_line( &mut buffer).expect("Should read fine");
 
     let init_request: HeaderMessage = serde_json::from_str(&buffer).expect("InitMessage");
-    let b = match init_request.body {
-        Body::Init(b) => b,
-        _ => panic!("not allowed"),
-    };
-    let init_response = HeaderMessage {src: init_request.dst, dst: init_request.src,
-        body: Body::InitOk(message::InitResponse { msg_id: b.msg_id, type_: "init_ok", in_reply_to: b.msg_id })
-    };
+    let init_response = init_request.respond();
 
     let json = serde_json::to_string(&init_response).expect("valid internal json");
     let json = format!("{json}\n");  
@@ -33,22 +27,13 @@ fn main() {
 
         handle.read_line( &mut buffer).expect("Should read fine");
 
-        let echo_request: HeaderMessage = serde_json::from_str(&buffer).expect("EchoMessage");
-
-        let b = match echo_request.body {
-            Body::Echo(e) => e,
-            _ => panic!("not allowed"),
-        };
-        let echo_response = HeaderMessage {
-            src: echo_request.dst,
-            dst: echo_request.src,
-            body: Body::EchoOk(message::EchoResponse { msg_id: b.msg_id, type_: "echo_ok", in_reply_to: b.msg_id, echo: b.echo })
-        };
+        let request: HeaderMessage = serde_json::from_str(&buffer).expect("Message");
+        let response = request.respond();
         
-        let json = serde_json::to_string(&echo_response).expect("valid internal json");
+        let json = serde_json::to_string(&response).expect("valid internal json");
         let json = format!("{json}\n");  
 
-        output.write(json.as_bytes()).expect("write ok Echo Response");
+        output.write(json.as_bytes()).expect("write ok Response");
 
         output.flush().unwrap();
     }
--- src/message.rs
+++ src/message.rs
@@ -1,20 +1,6 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
-#[serde(untagged)]
-#[serde(rename_all="snake_case")]
-pub enum Body<'a> {
-    #[serde(borrow)]
-    Init(InitRequest<'a>),
-    #[serde(borrow)]
-    InitOk(InitResponse<'a>),
-    #[serde(borrow)]
-    Echo(EchoRequest<'a>),
-    #[serde(borrow)]
-    EchoOk(EchoResponse<'a>),
-}
-
-#[derive(Serialize, Deserialize)]
 pub struct HeaderMessage<'a> {
     pub src: &'a str,
     #[serde(rename = "dest")]
@@ -22,20 +8,60 @@ pub struct HeaderMessage<'a> {
     pub body: Body<'a>,
 }
 
+impl<'a> HeaderMessage<'a> {
+    pub fn respond(self) -> Option<HeaderMessage<'a>> {
+
+        let resp:Option<(Payload<'_>, &'a str)> = match self.body.payload {
+            Payload::Init(_) => Some((Payload::InitOk(InitResponse { in_reply_to: self.body.msg_id }),"init_ok")),
+            Payload::InitOk(_) => None,
+            Payload::Echo(e) => Some((Payload::EchoOk(
+                EchoResponse { in_reply_to: self.body.msg_id, echo: e.echo }
+            ),"echo_ok")),
+            Payload::EchoOk(_) => None,
+        };
+
+        if let Some((payload, type_)) = resp {
+
+            Some(HeaderMessage { src: self.dst, dst: self.src, body: 
+                Body { msg_id: self.body.msg_id, type_, payload }
+            })
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
-pub struct InitRequest<'a> {
+#[serde(rename_all="snake_case")]
+pub struct Body<'a> {
     pub msg_id: isize,
     #[serde(rename = "type")]
     pub type_: &'a str,
+    #[serde(borrow, flatten)]
+    pub payload: Payload<'a>
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+#[serde(rename_all="snake_case")]
+pub enum Payload<'a> {
+    #[serde(borrow)]
+    Init(InitRequest<'a>),
+    InitOk(InitResponse),
+    #[serde(borrow)]
+    Echo(EchoRequest<'a>),
+    #[serde(borrow)]
+    EchoOk(EchoResponse<'a>),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct InitRequest<'a> {
     pub node_id: &'a str,
     pub node_ids: Vec<&'a str>, 
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct InitResponse<'a> {
-    pub msg_id: isize,
-    #[serde(rename = "type")]
-    pub type_: &'a str,
+pub struct InitResponse {
     pub in_reply_to: isize,
 }
 
@@ -50,17 +76,11 @@ pub struct InitResponse<'a> {
 
 #[derive(Serialize, Deserialize)]
 pub struct EchoRequest<'a> {
-    pub msg_id: isize,
-    #[serde(rename = "type")]
-    pub type_: &'a str,
     pub echo: &'a str,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct EchoResponse<'a> {
-    pub msg_id: isize,
-    #[serde(rename = "type")]
-    pub type_: &'a str,
     pub in_reply_to: isize,
     pub echo: &'a str,
 }
