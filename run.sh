0253b891deec530a81edc0c1c7123f19a3a7d789
Kasper Wind <kasperwind79@gmail.com>
2023-10-01 22:54:58 +0200

    update uid lib

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ./maelstrom/maelstrom test -w echo       --bin ~/repos/rust/rurtex/target/release/rurtex --node-count 1 --time-limit 10
-./maelstrom/maelstrom test -w unique-ids --bin ~/repos/rust/rurtex/target/release/rurtex --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+# ./maelstrom/maelstrom test -w echo       --bin ~/repos/rust/rurtex/target/release/rurtex --node-count 1 --time-limit 10 --concurrency 16
+./maelstrom/maelstrom test -w unique-ids --bin ~/repos/rust/rurtex/target/release/rurtex --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition --concurrency 16
